9f759ee616e73012bd88710452ed152b09fe98ee
thejsw <jswtncapetnc@gmail.com>
2025-03-11 11:14:46 +0900

    todolist crd

---
+++ todolist/src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+  // input value값으로 state 설정
+  const [toDo, setToDo] = useState("");
+  // toDos 배열로 선언
+  const [toDos, setToDos] = useState([]);
+
+  // input 값 변경 시 onChange 함수 실행
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  
+  // checked 속성을 바꿔줌
+  const onToggleCheck = (index) => {
+    setToDos((currentArray) => {
+      return currentArray.map((item, i) => {
+        if (i == index) {
+          return {...item, checked: !item.checked};
+        }
+        return item;
+      });
+    });
+  };
+
+  // toDos 배열 추가하는 함수
+  const onSubmit = (event) => {
+    // 재렌더링 시 새로고침 방지
+    event.preventDefault();
+    // 입력값 유효성 검사 / trim()으로 앞뒤 공백 제거 후 비어있지 않은지 확인
+    if (toDo.trim() !== "") {
+      setToDos((currentArray) => [...currentArray, 
+        {text: toDo, checked: false},
+      ]);
+      setToDo("");
+    }
+  };
+
+  // remove 함수
+  const onRemove = (index) => {
+    // onRemove이벤트로 받는 인자(index)와 i가 일치하면 제거
+    setToDos((currentArray) => currentArray.filter((_, i) => {
+      return i !== index
+    }));
+  };
+
+  return (
+    <>
+      <h1 className='title'>Todo List</h1>
+      <form className='form-box' onSubmit={onSubmit}>
+        <input id="todo" className='todo-input' type="text" value={toDo} onChange={onChange} />
+        <span id="addBtn" className='add-btn' role='button' onClick={onSubmit}></span>
+      </form>
+      <div id='todoBox' className='todo-box'>
+        {toDos.map((item, index) => (
+          <label className={`new-todo ${item.checked ? "check" : ""}`} key={index}>
+            {item.text}
+            <input type='checkbox' checked={item.checked} onChange={() => onToggleCheck(index)} />
+            <span className='rm-btn' onClick={() => onRemove(index)}></span>
+          </label>
+        ))}
+    </div>
+    </>
+  );
+}
+
+export default App;
